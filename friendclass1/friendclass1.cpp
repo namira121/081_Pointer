1516b8368bd7717c8e6cc686c2b7582a8f3ff59e
namira121 <127211008+namira121@users.noreply.github.com>
2023-05-10 11:50:58 +0700

    membuat friend function untuk class manusia

--- friendclass1/friendclass1.cpp
+++ friendclass1/friendclass1.cpp
@@ -16,7 +16,7 @@ public:
 	pelajar() {
 		nilai = 100;
 	}
-	friend void manusia::showNilaiPelajar(pelajar& x);
+	friend void manusia::showNilaiPelajar(pelajar& x); 
 };
 
 void manusia::showNilaiPelajar(pelajar& x) {
