6d45980901f76b4a7f3bf11bfd7fc691e4674aa4
namira121 <127211008+namira121@users.noreply.github.com>
2023-05-10 11:52:05 +0700

    membuat class mahasiswa

--- friendclass2/friendclass2.cpp
+++ friendclass2/friendclass2.cpp
@@ -1,20 +1,24 @@
-// friendclass2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa {
+private:
+	string nama;
+public:
+	friend void setNama(mahasiswa& a, string);
+};
+
+void setNama(mahasiswa& a, string pNama) {
+	a.nama = pNama;
+	cout << "Nama: " << a.nama;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	mahasiswa mhs;
+	setNama(mhs, "Joko Kumat");
+	cout << endl;
+	system("pause");
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
