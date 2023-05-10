3769c294ed049f7bfb45c1d01a12e9878256748f
namira121 <127211008+namira121@users.noreply.github.com>
2023-05-10 10:48:57 +0700

    main untuk menjalankan prosedur

--- 081_Pointer/081_Pointer.cpp
+++ 081_Pointer/081_Pointer.cpp
@@ -11,7 +11,7 @@ void mahasiswa::showNim() { //implementasi method diluar class
 	cout << "No Induk = " << nim << endl;
 }
 
-int main()
+int main() 
 {
 	mahasiswa mhs{ 1 };			//object mhs
 	mhs.showNim();				//Member Access Operator
