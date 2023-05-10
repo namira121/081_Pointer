f43c40d50514ef831525f5461f28477a480bb96b
namira121 <127211008+namira121@users.noreply.github.com>
2023-05-10 11:22:22 +0700

    membuat class siswa

---
+++ friendclass/friendclass.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+class siswa;
+
+class orang {
+private:
+	string nama;
+public:
+	void setNama(string pNama);
+	friend class siswa;
+};
+
+class siswa {
+private:
+	int id;
+public:
+	void setId(int pId);
+	void displayAll(orang& a);
+};
+
+void siswa::displayAll(orang& a) {
+	cout << id << endl << a.nama;
+}
+
+void siswa::setId(int pId) {
+
+}
